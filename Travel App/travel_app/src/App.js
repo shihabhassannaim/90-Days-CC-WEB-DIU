4ec540ea083a8a0bb4ff0603015f197ad340b2bd
shihabhassannaim <hassanshihab231@gmail.com>
2023-09-19 11:45:58 +0600

    Day-9 , 19 / 9 / 2023

--- Travel App/travel_app/src/App.js
+++ Travel App/travel_app/src/App.js
@@ -1,13 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import Home from './Components/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      
-      <i class="bg-info"></i>
-
      <Home />
     </div>
   );
--- Travel App/travel_app/src/Components/Header/Header.js
+++ Travel App/travel_app/src/Components/Header/Header.js
@@ -4,14 +4,14 @@ import logo from '../../images/whiteLogo.png'
 
 const Header = () => {
     return (
-        <div className='navbar'>
-                <div className='logo'><img src={logo} alt="" /></div>
-                <input className='search-bar' type="text" placeholder='Search Your Destination...'/>
-                <div className="header-option"><p>NEWS</p></div>
-                <div className="header-option"><p>DESTINATION</p></div>
-                <div className="header-option"><p>BLOG</p></div>
-                <div className="header-option"><p>CONTACT</p></div>
-                <div className="header-option"><button className=' login-btn'>Login</button></div>
+        <div className='navbar pt-4 flex ml-32'>
+                <div className='logo ml-10 mt-2.5 mr-20'><img className='w-20' src={logo} alt="" /></div>
+                <input className='search-bar bg-transparent border-solid border-2 border-green-300 rounded-xl w-80 h-10 align-middle mt-2 pl-5' type="text" placeholder='Search Your Destination...'/>
+                <div className="header-option py-4"><p>NEWS</p></div>
+                <div className="header-option py-4"><p>DESTINATION</p></div>
+                <div className="header-option py-4"><p>BLOG</p></div>
+                <div className="header-option py-4"><p>CONTACT</p></div>
+                <div className="header-option py-2.5"><button className='login-btn w-20 h-9 rounded-md bg-cyan-500 hover:bg-cyan-600 font-bold'>LOGIN</button></div>
         </div>
         
     );
